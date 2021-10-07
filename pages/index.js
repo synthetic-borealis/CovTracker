6874be1193652dbbe2ffe08cd666401be2373a4d
Frazonar <snip.coloured@gmail.com>
2021-10-07 20:08:21 +0300

    feat: add second news article

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,11 @@ import Panel from "../components/Panel.js";
 // News article panels
 const vitaminANewsPanel = new Panel({panelID: "article-vitamin-a"});
 vitaminANewsPanel.setEventListeners();
+const vaccinationNewsPanel = new Panel({panelID: "article-vaccination"});
+vaccinationNewsPanel.setEventListeners();
 
 // News cards
 const vitaminANewsButton = new Button({ buttonID: "vitamin-a-news-card" }, vitaminANewsPanel.open);
 vitaminANewsButton.setEventListeners();
+const vaccinationNewsButton = new Button({ buttonID: "vaccination-news-card" }, vaccinationNewsPanel.open);
+vaccinationNewsButton.setEventListeners();
